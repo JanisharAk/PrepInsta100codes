361a44fef7602e791121c1f6da4504ac74de7ca6
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-10-30 23:39:07 +0530

    code-03 sumFirstNnaturalNum

---
+++ src/playWithNumber/sumFirstNnaturalNum.java
@@ -0,0 +1,25 @@
+package playWithNumber;
+
+import java.util.Scanner;
+
+public class sumFirstNnaturalNum {
+
+    //We will find Sum of First N Natural Numbers in Java
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int num1 = sc.nextInt();
+        int sum = 0;
+        for(int i = 1; i <= num1; i++){
+            sum = sum + i;// stroing th value to sum
+        }
+        System.out.println("The sum is: " + sum);
+    }
+}
+/*
+Enter the first number:
+4
+The sum is: 10
+4 = 1 + 2 + 3 + 4 : 10
+ */
+
