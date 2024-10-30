84b9a3f0cc797151b5cee7e8cb60a05fa2cb536a
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-10-31 00:55:01 +0530

    code-06 GreatestOfTwoNum

---
+++ src/playWithNumber/GreatestOfTwoNum.java
@@ -0,0 +1,30 @@
+package playWithNumber;
+
+import java.util.Scanner;
+
+public class GreatestOfTwoNum {
+    public static void main(String[] args) {
+        /*
+        we will identify the greatest of two number
+         */
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the second number");
+        int num2 = sc.nextInt();
+//        System.out.println("=======first method============");
+
+//        int greatest = num1 > num2 ? num1 : num2;
+//        System.out.println("The greatest number is " + greatest);
+
+//        System.out.println("=======Second method============");
+
+        if(num1 > num2){
+            System.out.println(num1 + " is greater than " + num2);
+        }
+        else if(num1 < num2){
+            System.out.println(num2 + " is greater than " + num1);
+        }
+
+    }
+}
