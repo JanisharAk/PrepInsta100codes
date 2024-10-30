fc09a1dd21fd0329f2d47f09e1bef2c1f34fcfed
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-10-31 01:00:24 +0530

    code-07 GreatestOfThreeNum

---
+++ src/playWithNumber/GreatestOfThreeNum.java
@@ -0,0 +1,28 @@
+package playWithNumber;
+
+import java.util.Scanner;
+
+public class GreatestOfThreeNum {
+    /*
+    we will find greatest of three digit using if else condition
+    and comparing each one with another two
+     */
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter three numbers: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        if(num1 > num2 && num1 > num3){
+            System.out.println(num1 + " is greater than num2 and num3" );
+        }
+        else if(num2 > num1 && num2 > num3){
+            System.out.println(num2 + " is greater than num1 and num3" );
+        }
+        else if(num3 > num1 && num3 > num2){
+            System.out.println(num3 + " is greater than num1 and num2" );
+        }
+    }
+}
