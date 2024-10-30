57a036396bb8f849e9de0f9736cd6284cf2c6bff
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-10-30 23:37:26 +0530

    code-02 evenOddNum

---
+++ src/playWithNumber/evenOddNum.java
@@ -0,0 +1,30 @@
+package playWithNumber;
+
+import java.util.Scanner;
+
+public class evenOddNum {
+    public static void main(String[] args) {
+
+        /*
+        here we are using scanner classes for taking any input
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Even number");
+        }
+        else {
+            System.out.println("Odd number");
+        }
+    }
+}
+/*
+Enter the number:
+4
+even number
+Enter the number:
+3
+Odd number
+ */
+
