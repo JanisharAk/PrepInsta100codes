e6eb4a8983b2923b923cec0870986f6d47c252f0
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-11-05 00:10:49 +0530

    code-19 PowerOfaNumber

---
+++ src/playWithNumber/PowerOfaNumber.java
@@ -0,0 +1,23 @@
+package playWithNumber;
+
+import java.util.Scanner;
+
+public class PowerOfaNumber {
+    /*
+    Calculate the power of a number (base^power)
+    */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base integer: ");
+        int base = sc.nextInt();
+        System.out.print("Enter power integer: ");
+        int power = sc.nextInt();
+
+        int result = 1;
+        for (int i = 0; i < power; i++) {
+            result *= base; // Multiply result by base each time
+        }
+        System.out.println(base + " raised to the power of " + power + " is: " + result);
+        sc.close();
+    }
+}
