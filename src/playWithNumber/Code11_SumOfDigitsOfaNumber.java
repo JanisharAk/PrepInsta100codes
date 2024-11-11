8f142ae0ae0f6ad3bd17d7d2b7b89741c6f98011
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-11-11 23:16:26 +0530

    Code11_SumOfDigitsOfaNumber

---
+++ src/playWithNumber/Code11_SumOfDigitsOfaNumber.java
@@ -0,0 +1,15 @@
+package playWithNumber;
+
+public class Code11_SumOfDigitsOfaNumber {
+    public static void main (String[]args)
+    {
+        int num = 12345, sum = 0;
+        //loop to find sum of digits
+        while(num!=0){
+            sum += num % 10;
+            num = num / 10;
+        }
+        //output
+        System.out.println ("Sum of digits : " + sum);
+    }
+}
