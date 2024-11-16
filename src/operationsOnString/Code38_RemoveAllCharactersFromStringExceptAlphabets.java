7216bebb677b4ff22b6a9e5d51a27c1320612df3
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-11-16 21:14:30 +0530

    Code38_RemoveAllCharactersFromStringExceptAlphabets

---
+++ src/operationsOnString/Code38_RemoveAllCharactersFromStringExceptAlphabets.java
@@ -0,0 +1,26 @@
+package operationsOnString;
+
+import java.util.Scanner;
+
+
+public class Code38_RemoveAllCharactersFromStringExceptAlphabets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Using StringBuilder to store only alphabets
+        StringBuilder result = new StringBuilder();
+
+        // Iterate through each character of the input string
+        for (char c : input.toCharArray()) {
+            // Check if the character is an alphabet (uppercase or lowercase)
+            if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+                result.append(c);
+            }
+        }
+
+        // Print the resulting string with only alphabets
+        System.out.println("String with only alphabets: " + result.toString());
+    }
+}
