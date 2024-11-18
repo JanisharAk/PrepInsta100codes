a931473561faef59a2dbac6e7bda5f0bff859498
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-11-18 17:54:55 +0530

    Code47_ReplacingAParticularWordWithAnotherWordInString

---
+++ src/operationsOnString/Code47_ReplacingAParticularWordWithAnotherWordInString.java
@@ -0,0 +1,18 @@
+package operationsOnString;
+
+public class Code47_ReplacingAParticularWordWithAnotherWordInString {
+    /*
+
+     */
+    public static void main(String[] args) {
+        String originalString = "Java is a powerful programming language. Java is widely used.";
+        String wordToReplace = "Java";
+        String replacementWord = "Python";
+
+        // Using replace() method (case-sensitive)
+        String updatedString = originalString.replace(wordToReplace, replacementWord);
+
+        System.out.println("Original String: " + originalString);
+        System.out.println("Updated String: " + updatedString);
+    }
+}
