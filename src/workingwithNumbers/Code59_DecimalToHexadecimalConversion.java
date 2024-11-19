b62e7c754382cb1d70c21d40edd13e5f0140fe7c
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-11-19 23:50:16 +0530

    Code59_DecimalToHexadecimalConversion

---
+++ src/workingwithNumbers/Code59_DecimalToHexadecimalConversion.java
@@ -0,0 +1,52 @@
+package workingwithNumbers;
+
+import java.util.Scanner;
+
+public class Code59_DecimalToHexadecimalConversion {
+    /*
+    Enter a decimal number: 255
+    Output:The hexadecimal equivalent of decimal 255 is: FF
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input a decimal number
+        System.out.print("Enter a decimal number: ");
+        int decimal = sc.nextInt();
+
+        // Convert decimal to hexadecimal
+        String hex = Integer.toHexString(decimal).toUpperCase(); // Convert to uppercase for standard representation
+
+        // Output the hexadecimal result
+        System.out.println("The hexadecimal equivalent of decimal " + decimal + " is: " + hex);
+    }
+    /*
+    Scanner sc = new Scanner(System.in);
+
+        // Input a decimal number
+        System.out.print("Enter a decimal number: ");
+        int decimal = sc.nextInt();
+
+        // Convert decimal to hexadecimal manually
+        StringBuilder hex = new StringBuilder();
+        int number = decimal;
+
+        while (number > 0) {
+            int remainder = number % 16;
+            if (remainder < 10) {
+                hex.append(remainder); // Append numbers 0-9
+            } else {
+                hex.append((char) ('A' + (remainder - 10))); // Append letters A-F
+            }
+            number /= 16; // Update the number
+        }
+
+        // Reverse the hexadecimal string to get the correct order
+        hex.reverse();
+
+        // Output the hexadecimal result
+        System.out.println("The hexadecimal equivalent of decimal " + decimal + " is: " + hex);
+    }
+}
+     */
+}
