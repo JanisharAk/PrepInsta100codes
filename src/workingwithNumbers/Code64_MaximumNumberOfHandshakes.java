34481a02b7a3449356e9e48774842e84ba38308f
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-11-20 11:40:22 +0530

    Code64_MaximumNumberOfHandshakes

---
+++ src/workingwithNumbers/Code64_MaximumNumberOfHandshakes.java
@@ -0,0 +1,25 @@
+package workingwithNumbers;
+
+import java.util.Scanner;
+
+public class Code64_MaximumNumberOfHandshakes {
+
+    /*
+    Maximum Handshakes=(
+     (n/2)=n(n−1)/2
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input the number of people
+        System.out.print("Enter the number of people: ");
+        int n = sc.nextInt();
+
+        // Calculate the maximum number of handshakes
+        int maxHandshakes = (n * (n - 1)) / 2;
+
+        // Output the result
+        System.out.println("The maximum number of handshakes among " + n + " people is: " + maxHandshakes);
+    }
+
+}
