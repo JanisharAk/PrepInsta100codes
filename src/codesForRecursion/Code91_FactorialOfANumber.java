84de60aa909dd3898a2a3c35c89f615f6b0ad15b
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-12-06 23:57:44 +0530

    Code91_FactorialOfANumber

---
+++ src/codesForRecursion/Code91_FactorialOfANumber.java
@@ -0,0 +1,18 @@
+package codesForRecursion;
+
+import java.util.Scanner;
+
+public class Code91_FactorialOfANumber {
+    public static int factorial(int n) {
+        if(n==0) {
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = sc.nextInt();
+        System.out.println("Factorial of the number is: " + factorial(num));
+    }
+}
