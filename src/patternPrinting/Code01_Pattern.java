24699eb977ec82004f25dd4b2121af1b973aa5f8
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-12-28 20:59:14 +0530

    Code01_Pattern

---
+++ src/patternPrinting/Code01_Pattern.java
@@ -0,0 +1,19 @@
+package patternPrinting;
+
+public class Code01_Pattern {
+    /*
+     ****
+     ****
+     ****
+     ****
+     */
+    public static void main(String[] args) {
+        int row, col;
+        for (row = 1; row <= 5; row++) {
+            for (col = 1; col <= 5; col++) {
+                System.out.print( "*");
+            }
+            System.out.println();
+        }
+    }
+}
