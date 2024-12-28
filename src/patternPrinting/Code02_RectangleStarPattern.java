c175222bdd806ebd03b40612a5ab4ec3a699e6c0
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-12-28 21:06:57 +0530

    Code02_RectangleStarPattern

---
+++ src/patternPrinting/Code02_RectangleStarPattern.java
@@ -0,0 +1,19 @@
+package patternPrinting;
+
+public class Code02_RectangleStarPattern {
+    /*
+     ******
+     ******
+     ******
+     ******
+     */
+    public static void main(String[] args) {
+        int row,col;
+        for (row = 1; row <= 6; row++) {
+            for (col = 1; col <= 4; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
