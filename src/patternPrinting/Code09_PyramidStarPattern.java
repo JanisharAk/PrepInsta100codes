02c06a0f323adb5d386e314e34934bfc53971617
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-12-31 22:19:53 +0530

    Code09_PyramidStarPattern

---
+++ src/patternPrinting/Code09_PyramidStarPattern.java
@@ -0,0 +1,32 @@
+package patternPrinting;
+
+import java.util.Scanner;
+
+public class Code09_PyramidStarPattern {
+    /*
+
+        *
+       ***
+      *****
+     *******
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows in the pyramid: ");
+        int rows = sc.nextInt();
+        System.out.println("Enter the number of columns in the pyramid: ");
+       // int columns = sc.nextInt();
+        
+        for(int i = 1; i <= rows; i++){
+            // Print spaces before stars (leading spaces)
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
