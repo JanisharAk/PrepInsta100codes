dd6bfc8c9141a3338e1b65d93ae7f6aa13be072f
Janishar Akhtar <janisharakhtar277@gmail.com>
2025-01-05 22:35:40 +0530

    Code116_BasicSquare1Pattern

---
+++ src/patternPrinting/Code116_BasicSquare1Pattern.java
@@ -0,0 +1,24 @@
+package patternPrinting;
+
+import java.util.Scanner;
+
+public class Code116_BasicSquare1Pattern {
+    /*
+           1111
+           1111
+           1111
+           1111
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the square: ");
+        int n = sc.nextInt();
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                System.out.print("1");
+            }
+            System.out.println();
+        }
+
+    }
+}
