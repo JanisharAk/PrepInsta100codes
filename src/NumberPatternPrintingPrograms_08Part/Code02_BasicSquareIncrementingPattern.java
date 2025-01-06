5be9a61691032b0c0a3340ad6223be8228c9aba3
Janishar Akhtar <janisharakhtar277@gmail.com>
2025-01-06 19:15:41 +0530

    Code02_BasicSquareIncrementingPattern

---
+++ src/NumberPatternPrintingPrograms_08Part/Code02_BasicSquareIncrementingPattern.java
@@ -0,0 +1,24 @@
+package NumberPatternPrintingPrograms_08Part;
+
+import java.util.Scanner;
+
+public class Code02_BasicSquareIncrementingPattern {
+    /*
+           1111
+           2222
+           3333
+           4444
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the square: ");
+        int n = sc.nextInt();
+
+        for (int rows = 1; rows <= n; rows++) {
+            for (int cols = 1; cols <= n; cols++) {
+                System.out.print(rows);// Print the current row number
+            }
+            System.out.println();// Move to the next line after each row
+        }
+    }
+}
