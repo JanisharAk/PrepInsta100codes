6c5c821520f8780002faf178d95c81eb398ff2f0
Janishar Akhtar <janisharakhtar277@gmail.com>
2025-01-06 19:14:07 +0530

    code01_BasicSquare1Pattern

---
+++ src/NumberPatternPrintingPrograms_08Part/code01_BasicSquare1Pattern.java
@@ -0,0 +1,23 @@
+package NumberPatternPrintingPrograms_08Part;
+
+import java.util.Scanner;
+
+public class code01_BasicSquare1Pattern {
+    /*
+           1111
+           1111
+           1111
+           1111
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the square: ");
+        int n = sc.nextInt();
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                System.out.print("1");
+            }
+            System.out.println();
+        }
+    }
+}
