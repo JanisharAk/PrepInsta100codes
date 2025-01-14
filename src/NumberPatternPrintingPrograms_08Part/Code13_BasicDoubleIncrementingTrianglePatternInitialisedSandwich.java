d2b3926ecf05764df6bd4090516f94fd9e21f3b5
Janishar Akhtar <janisharakhtar277@gmail.com>
2025-01-14 20:59:58 +0530

    Code13_BasicDoubleIncrementingTrianglePatternInitialisedSandwich

---
+++ src/NumberPatternPrintingPrograms_08Part/Code13_BasicDoubleIncrementingTrianglePatternInitialisedSandwich.java
@@ -0,0 +1,53 @@
+package NumberPatternPrintingPrograms_08Part;
+
+import java.util.Scanner;
+
+public class Code13_BasicDoubleIncrementingTrianglePatternInitialisedSandwich {
+    /*
+            2
+            34
+            567
+            891011
+            891011
+            567
+            34
+            2
+              row = 4, initialize =2
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the rows number: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the initial number: ");
+        int initialize = sc.nextInt();
+
+        // Upper half
+        int num = initialize;
+        for (int i = 1; i <= rows; i++) {
+            int current = num;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(current);
+                current++;
+            }
+            num += i;
+            System.out.println();
+        }
+
+        // Lower half (Finally Corrected)
+        num = initialize + rows -1; // Correct initialization for lower half
+        for (int i = rows - 1; i >= 1; i--) {
+            int current = num;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(current);
+                current++;
+            }
+            num -= (i - 1);
+            System.out.println();
+        }
+
+        sc.close();
+
+    }
+
+
+}
