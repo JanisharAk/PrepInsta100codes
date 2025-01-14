48fe20225adc4e7d02c364e5afe108473b3d1b67
Janishar Akhtar <janisharakhtar277@gmail.com>
2025-01-14 20:36:00 +0530

    Code11_BasicIncrementingTrianglePatternInvertedInitialisedMirrored

---
+++ src/NumberPatternPrintingPrograms_08Part/Code11_BasicIncrementingTrianglePatternInvertedInitialisedMirrored.java
@@ -0,0 +1,40 @@
+package NumberPatternPrintingPrograms_08Part;
+
+public class Code11_BasicIncrementingTrianglePatternInvertedInitialisedMirrored {
+    /*
+           3
+           54
+           876
+           1211109
+           876
+           54
+           3
+     */
+    public static void main(String[] args) {
+        int rows = 4; // Number of rows in the upper triangle
+        int num = 3;
+
+        // Upper Triangle
+        for (int i = 1; i <= rows; i++) {
+            int temp = num;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(temp);
+                temp--;
+            }
+            num += i + 1;
+            System.out.println();
+        }
+
+        // Lower Triangle (Inverted)
+        num -= (rows + (rows + 1)); // Adjust num for the lower triangle
+        for (int i = rows - 1; i >= 1; i--) {
+            int temp = num;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(temp);
+                temp--;
+            }
+            num -= i;
+            System.out.println();
+        }
+    }
+}
