6c2257110161284b38f7ff6aa65042044c67d323
Janishar Akhtar <janisharakhtar277@gmail.com>
2025-02-23 21:59:21 +0530

    Code12_CountInversion

---
+++ src/Part12_AdvancedCodesRelatedToArrays/Code12_CountInversion.java
@@ -0,0 +1,45 @@
+package Part12_AdvancedCodesRelatedToArrays;
+
+public class Code12_CountInversion {
+    //we will use merge sort or Two pointer concept
+
+    public static int mergeAndCount(int[] arr, int left, int mid, int right) {
+        int[] leftArr = new int[mid - left + 1];
+        int[] rightArr = new int[right - mid];
+
+        for (int i = 0; i < leftArr.length; i++) leftArr[i] = arr[left + i];
+        for (int j = 0; j < rightArr.length; j++) rightArr[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left, swaps = 0;
+
+        while (i < leftArr.length && j < rightArr.length) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+                swaps += (mid + 1) - (left + i);  // Count inversions
+            }
+        }
+
+        while (i < leftArr.length) arr[k++] = leftArr[i++];
+        while (j < rightArr.length) arr[k++] = rightArr[j++];
+
+        return swaps;
+    }
+
+    public static int mergeSortAndCount(int[] arr, int left, int right) {
+        int count = 0;
+        if (left < right) {
+            int mid = (left + right) / 2;
+            count += mergeSortAndCount(arr, left, mid);
+            count += mergeSortAndCount(arr, mid + 1, right);
+            count += mergeAndCount(arr, left, mid, right);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 1, 3, 5};
+        System.out.println("Total Inversions: " + mergeSortAndCount(arr, 0, arr.length - 1)); // Output: 3
+    }
+}
